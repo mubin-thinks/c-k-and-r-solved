bf6934ad9b5b1e76ef67fde64d0cc4055aac75ce
mubin-thinks <mubin-thinks@proton.me>
2026-07-09 17:02:20 +0600

    added k&r style curly braces around while loop with multiple statement "looking" blocks.

--- src/chapter_1/exercise_1-10.c
+++ src/chapter_1/exercise_1-10.c
@@ -2,10 +2,11 @@
 
 int main() {
         int c;
-        while ((c = getchar()) != EOF)
+        while ((c = getchar()) != EOF) {
                 if (c == '\t') printf("\\t");
                 else if (c == '\b') printf("\\b");
                 else if (c == '\\') printf("\\\\");
                 else putchar(c);
+        }
         return 0;
 }
--- src/chapter_1/exercise_1-9.c
+++ src/chapter_1/exercise_1-9.c
@@ -2,12 +2,13 @@
 
 int main() {
         int c, is_space = 0;
-        while ((c = getchar()) != EOF)
+        while ((c = getchar()) != EOF) {
                 if (c == ' ') is_space = 1;
                 else if (is_space) {
                         putchar(' ');
                         putchar(c);
                         is_space = 0;
                 } else putchar(c);
+        }
         return 0;
 }
