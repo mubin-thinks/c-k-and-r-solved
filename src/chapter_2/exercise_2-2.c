3ee75999af7f675d9174748f352d08472e689ac7
mubin-thinks <mubin-thinks@proton.me>
2026-07-20 02:18:36 +0600

    added warning comment to exercise_2-2.c

--- src/chapter_2/exercise_2-2.c
+++ src/chapter_2/exercise_2-2.c
@@ -1,3 +1,19 @@
+// Using multiplication instead of the AND operation can
+// solve the problem.
+//
+// One thing to consider that in multiplication does not
+// stop after knowing the truthness of the condition.
+// meaning, if the first case is false, All the other
+// cases will be tested.
+//
+// This isn't problematic in this case as worst case assumes
+// we are calling getchar() after we found EOF, Which
+// results in getchar() keep return EOF afterwards. The
+// program works as intended.
+//
+// Usually you should not be using multiplication because of
+// the stated reason and prefer && or || when possible.
+
 #include <stdio.h>
 
 #define MAX_LINE_LENGTH 1000
