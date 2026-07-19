52ca64d26f7480684fad1a79383de7e9aeda21b5
mubin-thinks <mubin-thinks@proton.me>
2026-07-19 23:25:47 +0600

    improved exercise_2-2

--- src/chapter_2/exercise_2-2.c
+++ src/chapter_2/exercise_2-2.c
@@ -3,13 +3,10 @@
 #define MAX_LINE_LENGTH 1000
 
 int main() {
-        int i = 0;
+        int c, i = 0;
         char s[MAX_LINE_LENGTH];
-        for (
-                int c;
-                (i < MAX_LINE_LENGTH - 1) + ((c = getchar()) != '\n') + (c != EOF) == 3;
-                ++i
-        ) s[i] = c;
+        for (; (i < MAX_LINE_LENGTH - 1) * ((c = getchar()) != '\n') * (c != EOF); ++i)
+                s[i] = c;
         s[i] = '\0';
         printf("%s\n", s);
         return 0;
