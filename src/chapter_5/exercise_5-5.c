c0242ae3536003983dc8b8ac5281a9a3a7b72dc2
mubin-thinks <mubin-thinks@proton.me>
2026-08-02 12:27:21 +0600

    added tests for exercise_5-5

--- src/chapter_5/exercise_5-5.c
+++ src/chapter_5/exercise_5-5.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
-#include <string.h>
 
-char* my_strncpy(char* s, char* t, int n);
-char* my_strncat(char* s, char* t, int n);
-int my_strncmp(char* s, char* t, int n);
+char* strncpy(char* s, char* t, int n);
+char* strncat(char* s, char* t, int n);
+int strncmp(char* s, char* t, int n);
 
 int main() {
+        char s[1000];
+        printf("%s\n", strncpy(s, "example", 8));
+
+        strncpy(s, "this text needs to be null-terminated.", 4);
+        s[4] = '\0';
+        printf("%s\n", s);
+
+        printf("%s\n", strncpy(s, "a", 2));
+        printf("%s\n", strncpy(s, "", 1000));
+        printf("%s\n", strncpy(s, "test", 0));
+
+        strncat(s, "hello world!", 5);
+        strncat(s, " some other text.", 11);
+        strncat(s, " abcdefgh", 6);
+        strncat(s, " done.", 6);
+        strncat(s, "", 1000);
+        printf("%s\n", s);
+
+        printf("%d\n", strncmp(s, "hello", 5));
+        printf("%d\n", strncmp(s + 5, " some other", 11));
+        printf("%d\n", strncmp(s + 5, " some other", 5));
+        printf("%d\n", strncmp(s + 23, "done.", 5));
+        printf("%d\n", strncmp(s, "will not match", 5));
         return 0;
 }
 
-char* my_strncpy(char* s, char* t, int n) {
-        for (; n-- && *s++ = *t++; );
-        if (n > 0) for (; n--; ) *s++ = '\0';
+char* strncpy(char* s, char* t, int n) {
+        char *p = s;
+        for (; n-- && (*p++ = *t++); );
+        if (n > 0) for (; n--; ) *p++ = '\0';
+        return s;
 }
 
-char* my_strncat(char* s, char* t, int n) {
-        for (; *s != '\0'; s++);
-        for (; n-- && *s++ = *t++; );
-        if (*(s - 1) != '\0') *s = '\0';
+char* strncat(char* s, char* t, int n) {
+        char *p = s;
+        for (; *p != '\0'; p++);
+        for (; n-- && (*p++ = *t++); );
+        if (*(p - 1) != '\0') *p = '\0';
+        return s;
 }
 
-int my_strncmp(char* s, char* t, int n) {
-        for (; n-- && *s == *t; s++, t++)
-                if (*s == '\0') return 0;
+int strncmp(char* s, char* t, int n) {
+        for (; n-- && (*s == *t); s++, t++) if (*s == '\0' || n == 0) return 0;
         return *s - *t;
 }
